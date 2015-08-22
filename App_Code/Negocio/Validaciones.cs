5fd423283c6a02cff2a35ff99ae3577245f1dcd5
pipechava <pipechava@gmail.com>
2015-08-22 02:58:10 -0600

    todo

---
+++ App_Code/Negocio/Validaciones.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace AutoAgenciaCR_WebSite.App_Code.Negocio
+{
+    public class Validaciones
+    {
+        public Boolean isEmpty(String nombre, String apellido, String correo,
+            String direccion, int telefono, String contraseña, String usuario) {
+                if (nombre == "" || apellido == "" || correo == "" ||
+                   direccion == "" || telefono == null || contraseña == "" || usuario == "")
+                {
+                    return false;
+                }
+                else
+                {
+                    return true; 
+                }
+        }
+
+        public Boolean isName(String dato) {
+            if ( !Regex.Match(dato,"^[A-Z][a-zA-Z]*$" ).Success){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        public Boolean isEmail(String dato) { 
+            return true;
+        }
+    }
+}
---
+++ App_Code/UI/UIIndex.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoAgenciaCR_WebSite.App_Code.UI
+{
+	public class UIIndex
+	{
+
+        private Negocio.Validaciones v;
+        private Negocio.Administrador a;
+
+        public UIIndex()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+		public Boolean ValidarLogin(String usuario, String password)
+		{
+			bool condition = false;
+			a = new Negocio.Administrador();
+			if (a.ingresarUsuario(usuario, password))
+			{
+				condition = true;
+			}
+			else
+			{
+				condition = false;
+			}
+
+
+			return condition;
+		}
+
+		public Boolean validarRegistro(String nombre, String apellido, String correo,
+			String direccion, int telefono, String contraseña, String usuario)
+		{
+			bool condition = false;
+			//v.isEmpty(nombre, apellido, correo, direccion, telefono, contraseña, usuario);
+			//validar nombre-apellido-telefono-correo
+			//if condicion false entonces mando a admin
+
+			a = new Negocio.Administrador();
+			//bool condition=false;
+			a.registrarUsuario(nombre, apellido, correo, direccion, telefono, contraseña, usuario);
+			return condition;
+		}
+
+	}
+}
